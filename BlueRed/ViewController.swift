40730932785b8f5fe58bd539047bd1ba4c54e01b
Prince Bajracharya <Prince@Princes-MacBook-Pro.local>
2016-01-17 14:40:49 -0800

    First commit

--- BlueRed/ViewController.swift
+++ BlueRed/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImgFootball: UIImageView!
+    @IBOutlet weak var ImgBasketBall: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ImgFootball.hidden = true
+        ImgBasketBall.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func BtnBasketBall(sender: AnyObject) {
+        ImgBasketBall.hidden = false
+        ImgFootball.hidden = true
+    }
 
+    @IBAction func BtnFootball(sender: AnyObject) {
+        ImgBasketBall.hidden = true
+        ImgFootball.hidden = false
+    }
 }
 
